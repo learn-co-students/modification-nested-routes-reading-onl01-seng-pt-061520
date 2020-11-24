e9f185aea7a2896887e2c6c0318c31063c4c572d
Jade Lynn Masker <jadelynn@masker.family>
2020-11-24 15:45:02 -0500

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,9 @@
 module PostsHelper
-  
+  def author_id_field(f)
+    if f.object.author.nil?
+      f.select :author_id, Author.all, :id, :name
+    else
+      f.hidden_field :author_id, value: f.object.author_id
+    end
+  end
 end
